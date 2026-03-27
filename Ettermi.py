3e2165d49b5d14e3ef80584b1e01c2008cdd26c0
barnabasss3 <sumegibarnabas2009@gmail.com>
2026-03-27 19:31:59 +0100

    Refactor order handling and menu loading

--- Ettermi.py
+++ Ettermi.py
@@ -1,148 +1,215 @@
 class Rendeles:
-    def __init__(self, asztal, termek, db, statusz="függőben"):
-        self.asztal = asztal
-        self.termek = termek
-        self.db = db
-        self.statusz = statusz
+    def __init__(self, asztal, termek_db=[], statusz="függőben"):
+        self.asztal=asztal
+        self.termek_db=termek_db
+        self.statusz=statusz
 
-    def kiir(self, fajl):
-        fajl.write(f"{self.asztal};{self.termek};{self.db};{self.statusz}\n")
+    def hozzaad(self, termek, db):
+        self.termek_db.append((termek, db))
+
+    def zar(self):
+        self.statusz="lezárt"
 
 
 # Menü betöltése
-menu = []
-with open("menu.csv", "r", encoding="utf-8") as menu_ki:
-    for sor in menu_ki:
-        menu.append(sor.strip().split(";"))
+def menu_betolt():
+    menu=[]
+    with open("menu.csv", "r", encoding="utf-8") as sorok:
+        for sor in sorok:
+            sor=sor.strip().split(";")
+            if len(sor)<3:
+                continue
+            sor[2]=int(sor[2])
+            menu.append((sor[0], sor[1], sor[2]))
+    return menu
 
 
-# Vásárlások fájl
-with open("vasarlasok.csv", "w", encoding="utf-8") as f:
-    f.write("asztal;termék;db;státusz\n")
+# raktár betöltése
+def raktar_betolt():
+    raktar=[]
+    with open("raktar.csv", "r", encoding="utf-8") as sorok:
+        for sor in sorok:
+            sor=sor.strip().split(";")
+            if len(sor)<2:
+                continue
+            sor[1]=int(sor[1])  
+            raktar.append((sor[0], sor[1]))         
+    return raktar
 
-vasarlasok_kiiratas = open("vasarlasok.csv", "a", encoding="utf-8")
+
+# Vásárlások betöltése
+# Vásárló neve;Felszolgáló neve;függőben/pénz;termékek
+def vasarlasok_betolt():
+    vasarlasok=[]
+    with open("vasarlasok.csv", "r", encoding="utf-8") as sorok:
+        for sor in sorok:
+            sor=sor.strip().split(";")
+            if len(sor)<4:
+                continue
+            if sor[2].isdigit():
+                sor[2]=int(sor[2]) 
+            vasarlasok.append((sor[0], sor[1], sor[2], sor[3]))         
+    return vasarlasok
 
 
-muvelet = input("Mit szeretnél módosítani? (rendelés, menü) ")
+def raktar_ment(raktar):
+    with open("raktar.csv", "w", encoding="utf-8") as sorok:
+        for nev, db in raktar:
+            sorok.write(f"{nev};{db}\n")
 
-# RENDELÉS
 
-if muvelet == "rendelés":
-    rendeles = input("Mit szeretnél csinálni (új, vége) ")
+def raktar_csokkent(termek, db, raktar):
+    with open("recept.csv", "r", encoding="utf-8") as f:
+        for sor in f:
+            etel, alapanyag, menny=sor.strip().split(";")
+            menny=int(menny)
+            
+            if etel==termek:
+                for i in range(len(raktar)):
+                    if raktar[i][0]==alapanyag:
+                        nev, jelenlegi_db=raktar[i]
+                        raktar[i]=(nev, jelenlegi_db-menny*db)
+                        break
 
-    if rendeles == "új":
-        szam = int(input("Melyik asztal? "))
-        while not 1 <= szam <= 12:
-            szam = int(input("Melyik asztal? (1-12) "))
+
+menu=menu_betolt()
+raktar=raktar_betolt()
+rendelesek=vasarlasok_betolt()
+
+muvelet=input("Mit szeretnél módosítani? (rendelés, menü) ")
+
+
+# RENDELÉS 
+
+if muvelet=="rendelés":
+    felszolgalo_neve=input("Add meg a neved: ")
+    vasarlo_neve=input("Vásárló neve: ")
+    rendeles=input("Mit szeretnél csinálni (új/hozzáadás, vége) ")
+
+    # ÚJ / HOZZÁADÁS
+    if rendeles=="új" or rendeles=="hozzáadás":
+
+        termekek_lista=[]
 
         # ital
-        if input("Szeretnél italt? (igen/nem) ") == "igen":
+        if input("Ital: (igen/nem) ")=="igen":
             while True:
-                ital = input("Ital (víz, fanta, cola) 0=kilépés: ")
-                if ital == "0":
+                ital=input("Ital (0=kilépés): ")
+                if ital=="0":
                     break
-                db = int(input("Darab: "))
-                if db > 0:
-                    Rendeles(szam, ital, db).kiir(vasarlasok_kiiratas)
+                db=int(input("Darab: "))
+                if db>0:
+                    termekek_lista.append(f"{ital}({db})")
+                    raktar_csokkent(ital, db, raktar)
 
         # étel
-        if input("Szeretnél ételt? (igen/nem) ") == "igen":
+        if input("Étel: (igen/nem) ")=="igen":
             while True:
-                etel = input("Étel (pizza, hamburger, gyros, saláta) 0=kilépés: ")
-                if etel == "0":
+                etel=input("Étel (0=kilépés): ")
+                if etel=="0":
                     break
-                db = int(input("Darab: "))
-                if db > 0:
-                    Rendeles(szam, etel, db).kiir(vasarlasok_kiiratas)
+                db=int(input("Darab: "))
+                if db>0:
+                    termekek_lista.append(f"{etel}({db})")
+                    raktar_csokkent(etel, db, raktar)
 
         # desszert
-        if input("Szeretnél desszertet? (igen/nem) ") == "igen":
+        if input("Desszert: (igen/nem) ")=="igen":
             while True:
-                desszert = input("Desszert 0=kilépés: ")
-                if desszert == "0":
+                desszert=input("Desszert 0=kilépés: ")
+                if desszert=="0":
                     break
-                db = int(input("Darab: "))
-                if db > 0:
-                    Rendeles(szam, desszert, db).kiir(vasarlasok_kiiratas)
+                db=int(input("Darab: "))
+                if db>0:
+                    termekek_lista.append(f"{desszert}({db})")
+                    raktar_csokkent(desszert, db, raktar)
 
-    elif rendeles == "vége":
-        asztal = int(input("Melyik asztal fizet? "))
+        termekek_szoveg=",".join(termekek_lista)
 
-        with open("vasarlasok.csv", "r", encoding="utf-8") as f:
-            sorok = f.readlines()
+        with open("vasarlasok.csv", "a", encoding="utf-8") as f:
+            f.write(f"{vasarlo_neve};{felszolgalo_neve};függőben;{termekek_szoveg}\n")
 
-        osszeg = 0
-        print("\nFizetendő tételek:")
+        raktar_ment(raktar)
 
-        for sor in sorok[1:]:
-            adat = sor.strip().split(";")
+    #LEZÁRÁS
+    elif rendeles=="vége":
 
-            if int(adat[0]) == asztal:
-                termek = adat[1]
-                db = int(adat[2])
+        uj_lista=[]
 
-                for m in menu:
-                    if m[1] == termek:  
-                        ar = int(m[2])   
-                        resz = ar * db
-                        osszeg += resz
-                        print(f"{termek} x{db} = {resz} Ft")
+        for sor in rendelesek:
+            if sor[0]==vasarlo_neve and sor[2]=="függőben":
 
-        print(f"Összesen: {osszeg} Ft")
+                osszeg=0
+                print("\nFizetendő tételek:")
+
+                termekek=sor[3].split(",")
+
+                for t in termekek:
+                    nev, db = t.replace(")", "").split("(")
+                    db=int(db)
+
+                    for m in menu:
+                        if m[1]==nev:
+                            resz=m[2]*db
+                            osszeg+=resz
+                            print(f"{nev} x{db} = {resz} Ft")
+
+                print(f"Összesen: {osszeg} Ft")
+
+                uj_lista.append((sor[0], sor[1], osszeg, sor[3]))
+            else:
+                uj_lista.append(sor)
+
+        with open("vasarlasok.csv", "w", encoding="utf-8") as f:
+            for sor in uj_lista:
+                f.write(f"{sor[0]};{sor[1]};{sor[2]};{sor[3]}\n")
 
 
+#MENÜ
 
-# MENÜ
+elif muvelet=="menü":
+    muv=input("Mit szeretnél csinálni? (új/törlés) ")
 
-elif muvelet == "menü":
-    muv = input("Mit szeretnél csinálni? (új, törlés) ")
+    if muv=="új":
+        kat=input("Kategória (ital/fő/dessz): ")
+        nev=input("Termék neve: ")
 
-    if muv == "új":
-        kat = input("Kategória (ital, fő, dessz): ")
-        nev = input("Termék neve: ")
-
-        # ellenőrzés hogy létezik-e
-        letezik = False
+        letezik=False
         for sor in menu:
-            if sor[1] == nev:
-                letezik = True
+            if sor[1]==nev:
+                letezik=True
 
         if letezik:
             print("Ilyen már van!")
         else:
-            ar = input("Ár: ")
-
-            # recept ha nem ital
-            if kat != "ital":
-                alapanyagok = ["paradicsom", "olíviabogyó", "szósz", "hús", "vegyessaláta", "liszt"]
-
+            ar=input("Ár: ")
+            if kat!="ital":
                 while True:
-                    hozz = input("Hozzávaló (0=kilépés): ")
-                    if hozz == "0":
+                    hozz=input("Hozzávaló (0=kilép): ")
+                    if hozz=="0":
                         break
-                    if hozz not in alapanyagok:
-                        print("Csak a listából válassz!")
-                        continue
+                    menny=input("Mennyiség: ")
+                    with open("recept.csv", "a", encoding="utf-8") as recept_fajl:
+                        recept_fajl.write(f"{nev};{hozz};{menny}\n")
 
-                    menny = input("Mennyiség: ")
+            with open("menu.csv", "a", encoding="utf-8") as menu_fajl:
+                menu_fajl.write(f"{kat};{nev};{ar}\n")
 
-                    with open("recept.csv", "a", encoding="utf-8") as r:
-                        r.write(f"{nev};{hozz};{menny}\n")
+    elif muv=="törlés":
+        nev=input("Mit szeretnél törölni? ")
 
-            # menübe írás
-            with open("menu.csv", "a", encoding="utf-8") as f:
-                f.write(f"\n{kat};{nev};{ar}")
+        with open("menu.csv", "r", encoding="utf-8") as fajl:
+            sorok=fajl.readlines()
 
-    elif muv == "törlés":
-        nev = input("Mit szeretnél törölni? ")
-
-        with open("menu.csv", "r", encoding="utf-8") as f:
-            sorok = f.readlines()
-
-        with open("menu.csv", "w", encoding="utf-8") as f:
+        with open("menu.csv", "w", encoding="utf-8") as fajl:
             for sor in sorok:
-                if sor.split(";")[1] != nev:
-                    f.write(sor)
+                if sor.split(";")[1]!=nev:
+                    fajl.write(sor)
 
-
-vasarlasok_kiiratas.close()
+        with open("recept.csv", "r", encoding="utf-8") as fajl:
+            sorok=fajl.readlines()
+        
+        with open("recept.csv", "w", encoding="utf-8") as fajl:
+            for sor in sorok:
+                if sor.split(";")[0]!=nev:
+                    fajl.write(sor)
